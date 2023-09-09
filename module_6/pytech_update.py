6b95b631e214f52b25d540c2292f6e05d053f697
Juan <juarodriguez3@my365.bellevue.edu>
2023-09-08 23:32:20 -0500

    update Module 6

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -5,7 +5,7 @@ def output_student(student):
     print("First Name: " + student['first_name'])
     print("Last Name: " + student['last_name'])
     print("")
-    
+
 
 url="mongodb+srv://admin:admin@cluster0.vkti0bu.mongodb.net/?retryWrites=true&w=majority"
 client= MongoClient(url)
@@ -13,14 +13,17 @@ db=client.pytech
 students= db.students
 
 
+results= students.update_one({'student_id':1007}, {"$set":{'last_name':'Rodriguez'}})
+find_student= students.find_one({'student_id':1007})
+
 all_students= db.students.find()
 print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY -- ")
 for student in all_students:
     output_student(student)
 
-students.update_one({'student_id':1007}, {"$set":{"last_name":"Looper"}})
-find_student= students.find_one({"student_id":1007})
+
 print(" -- DISPLAYING STUDENT DOCUMENT 1007 -- ")
-output_student(find_student)
+
+output_student(find_student) 
 
 input('End of program, press any key to exit...')
