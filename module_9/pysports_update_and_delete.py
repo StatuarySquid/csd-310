770dc04b0e8c95f1380c336bc43797747d97e364
Juan <juarodriguez3@my365.bellevue.edu>
2023-09-20 11:30:52 -0500

    working code for insert

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -14,18 +14,18 @@ def display_results(x):
         print('Team Name: {}'.format(x[3]))
         print()
     
-
+'''INNER JOIN FOR CORRECT FORMAT'''
 cursor = connection.cursor()
 query="SELECT player.player_id, player.first_name, player.last_name, team.team_name FROM player INNER JOIN team ON player.team_id=team.team_id"
 
 cursor.execute(query)
 rows=cursor.fetchall()
 
+
 '''INSERT NEW PLAYER'''
 insert_sql = "INSERT INTO player (player_id, first_name, last_name, team_id) VALUES (%s, %s, %s, %s)"
 new_player= ("021", "Smeagol", "Shire Folk", "1")
-cursor.execute(insert_sql,new_player)
-connection.commit()
+
 
 print('--DISPLAYING PLAYERS AFTER INSERT--')
 display_results(insert_sql)
@@ -35,16 +35,14 @@ display_results(insert_sql)
 
 print('DISPLAYING PLAYERS AFTER UPDATE')
 update_sql= "UPDATE player SET player_id= '21', team_id='2' WHERE first_name='Smeagol'"
-cursor.execute(update_sql)
-connection.commit()
+
 display_results(update_sql)
 
 '''DELETE PLAYER'''
 
 print("--DISPLAYING PLAYERS AFTER DELETE")
 delete_player= "DELETE FROM player WHERE first_name='Smeagol'"
-cursor.execute(delete_player)
-connection.commit()
+
 display_results(delete_player)
 
 
