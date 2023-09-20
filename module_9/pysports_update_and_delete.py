e06e2901c5f3d9ac2384db4d7359944ded3cb195
Juan <juarodriguez3@my365.bellevue.edu>
2023-09-20 12:04:04 -0500

    ready for review

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -9,28 +9,23 @@ db='pysports')
 cursor = connection.cursor()
 
 
-def display_results(x):
-    
-    for x in rows:  
-        print('Player ID: {}'.format(x[0]))
-        print('First Name: {}'.format(x[1]))
-        print('Last Name: {}'.format(x[2]))
-        print('Team Name: {}'.format(x[3]))
+def display_results(query):
+    query="SELECT player.player_id, player.first_name, player.last_name, team.team_name FROM player INNER JOIN team ON player.team_id=team.team_id"
+    cursor.execute(query)
+    rows=cursor.fetchall()
+    for query in rows:  
+        print('Player ID: {}'.format(query[0]))
+        print('First Name: {}'.format(query[1]))
+        print('Last Name: {}'.format(query[2]))
+        print('Team Name: {}'.format(query[3]))
         print()
     
-'''INNER JOIN FOR CORRECT FORMAT'''
-
-query="SELECT player.player_id, player.first_name, player.last_name, team.team_name FROM player INNER JOIN team ON player.team_id=team.team_id"
-
-cursor.execute(query)
-rows=cursor.fetchall()
-
 
 '''INSERT NEW PLAYER'''
 insert_sql = "INSERT INTO player (player_id, first_name, last_name, team_id) VALUES (%s, %s, %s, %s)"
 new_player= ("021", "Smeagol", "Shire Folk", "1")
 cursor.execute(insert_sql,new_player)
-connection.commit()
+
 
 print('--DISPLAYING PLAYERS AFTER INSERT--')
 display_results(insert_sql)
@@ -39,14 +34,18 @@ display_results(insert_sql)
 '''UPDATE PLAYER''' 
 
 print('DISPLAYING PLAYERS AFTER UPDATE')
-update_sql= "UPDATE player SET player_id= '21', team_id='2' WHERE first_name='Smeagol'"
+update_sql= "UPDATE player SET player_id= '21',first_name='Gollum', last_name='Ring Stealer', team_id='2' WHERE first_name='Smeagol'"
+
+cursor.execute(update_sql)
 
 display_results(update_sql)
 
 '''DELETE PLAYER'''
 
 print("--DISPLAYING PLAYERS AFTER DELETE")
-delete_player= "DELETE FROM player WHERE first_name='Smeagol'"
+delete_player= "DELETE FROM player WHERE first_name='Gollum'"
+
+cursor.execute(delete_player)
 
 display_results(delete_player)
 
