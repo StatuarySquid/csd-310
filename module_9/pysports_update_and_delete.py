132a70e5fda050e75d5f23232f2dc7a5acd922af
Juan <juarodriguez3@my365.bellevue.edu>
2023-09-20 10:59:19 -0500

    start of the code

---
+++ module_9/pysports_update_and_delete.py
@@ -0,0 +1,53 @@
+import mysql.connector
+from mysql.connector import Error
+
+connection = mysql.connector.connect(host="localhost",
+user='root2',
+passwd='cybr41018!',
+db='pysports')
+
+def display_results(x):
+    for x in rows:  
+        print('Player ID: {}'.format(x[0]))
+        print('First Name: {}'.format(x[1]))
+        print('Last Name: {}'.format(x[2]))
+        print('Team Name: {}'.format(x[3]))
+        print()
+    
+
+cursor = connection.cursor()
+query="SELECT player.player_id, player.first_name, player.last_name, team.team_name FROM player INNER JOIN team ON player.team_id=team.team_id"
+
+cursor.execute(query)
+rows=cursor.fetchall()
+
+'''INSERT NEW PLAYER'''
+insert_sql = "INSERT INTO player (player_id, first_name, last_name, team_id) VALUES (%s, %s, %s, %s)"
+new_player= ("021", "Smeagol", "Shire Folk", "1")
+cursor.execute(insert_sql,new_player)
+connection.commit()
+
+print('--DISPLAYING PLAYERS AFTER INSERT--')
+display_results(insert_sql)
+
+
+'''UPDATE PLAYER''' 
+
+print('DISPLAYING PLAYERS AFTER UPDATE')
+update_sql= "UPDATE player SET player_id= '21', team_id='2' WHERE first_name='Smeagol'"
+cursor.execute(update_sql)
+connection.commit()
+display_results(update_sql)
+
+'''DELETE PLAYER'''
+
+print("--DISPLAYING PLAYERS AFTER DELETE")
+delete_player= "DELETE FROM player WHERE first_name='Smeagol'"
+cursor.execute(delete_player)
+connection.commit()
+display_results(delete_player)
+
+
+connection.close()
+
+input("Click to continue...")
