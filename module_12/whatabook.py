f78558c5fd0f7beac5e3e8b5d934a6612dd80a3e
StatuarySquid <141707098+StatuarySquid@users.noreply.github.com>
2023-10-03 23:41:15 -0500

    fixed errors and wrote comments

--- module_12/whatabook.py
+++ module_12/whatabook.py
@@ -2,6 +2,7 @@ import sys
 import mysql.connector
 from mysql.connector import Error
 
+#\start of the connection for the functions 
 my_db= mysql.connector.connect(
     host='localhost',
     user='root',
@@ -10,7 +11,8 @@ my_db= mysql.connector.connect(
 
 _cursor=my_db.cursor()
 
-"Functions "
+#\ All functions start here
+#\ show_menu()- display menu and ask which menu to go next
 
 def show_menu():
     print("\n Welcome to our menu")
@@ -26,6 +28,7 @@ def show_menu():
         
         sys.exit(0)
 
+#\show_books is supposed to display all 9 books 
 def show_books(_cursor):
     _cursor.execute("SELECT book_id, book_name, author ,details FROM book;")
     book_result= _cursor.fetchall()
@@ -33,7 +36,7 @@ def show_books(_cursor):
     for book in book_result:
         print("  Book Name: {}\n  Author: {}\n  Details: {}\n".format(book[0], book[1], book[2]))
         
-
+#\show_location display the location address
 def show_locations(_cursor):
     _cursor.execute("Select store_id,locale from store;")
     store_result=_cursor.fetchall()
@@ -42,33 +45,37 @@ def show_locations(_cursor):
         
         sys.exit(0)
     
-
+#\validate_user is supposed to validate the user's identity since the are only three user id 1-3
 def validate_user(): 
+    try:
+        user_input_id= int(input('Please provide us with your user ID: '))
 
-    while True:
-        try:
-            user_input_id= int(input('Please provide us with your user ID: '))
+        if user_input_id <= 0 or user_input_id > 3:
+            print('Invalid ID')
+            sys.exit(0)
+        return user_input_id
+    except ValueError: 
+        print('That is an invalid input.')
+        
+        sys.exit(0)
 
-            if user_input_id <= 0 or user_input_id > 3:
-                print('Invalid ID')
-                sys.exit(0)
-            return user_input_id
-        except ValueError: 
-            print('That is an invalid input.')
-            continue
-
+#\ show_account_menu() once a user's id is validated
+#\it will display their Wishlist
+#\Add book or return to main menu
 def show_account_menu():
+    try:
+        print('\nAccount Menu \n1. Wishlist \n2. Add Book \n3. Main Menu ')
+        user_input_account= int(input('Enter number of menu:  '))
+        return user_input_account
     
-    while True:
-        try:
-            print('\nAccount Menu \n1. Wishlist \n2. Add Book \n3. Main Menu ')
-            user_input_account= int(input('Enter number of menu:  '))
-            return user_input_account
-        except ValueError:
-            print("That is invalid number")
-            continue
+    except ValueError:
+        print("That is invalid number")
+        
+        sys.exit(0)
     
-    #/ Was not able to get this function to work correctly had to copy and paste it from github repository/#
+#\ Was not able to get this function to work correctly 
+#\ had to copy and paste it from github repository
+#\ show user's wishlist but was infinite 
 def show_wishlist(_cursor, _user_id):
     """ query the database for a list of books added to the users wishlist """
 
@@ -85,7 +92,7 @@ def show_wishlist(_cursor, _user_id):
     for book in wishlist:
         print("        Book Name: {}\n        Author: {}\n".format(book[4], book[5]))
 
-
+#\ show books not in a user's wishlist
 def show_books_to_add(_cursor, _user_id):
     """ query the database for a list of books not in the users wishlist """
 
@@ -95,20 +102,20 @@ def show_books_to_add(_cursor, _user_id):
 
     print(query)
 
-    cursor.execute(query)
+    _cursor.execute(query)
 
     books_to_add = _cursor.fetchall()
 
-    print("\n        -- DISPLAYING AVAILABLE BOOKS --")
+    print("\n Available Books")
 
     for book in books_to_add:
         print("        Book Id: {}\n        Book Name: {}\n".format(book[0], book[1], book[2], book[3]))
-
+#\add_book_to_wishlist is supposed to add to a user's wishlist
 def add_book_to_wishlist(_cursor, _user_id, _book_id):
-    _cursor.execute("INSERT INTO wishlist(wishlist_id,user_id, book_id) VALUES({}, {})".format(_user_id, _book_id))
+    _cursor.execute("INSERT INTO wishlist(user_id, book_id) VALUES({}, {})".format(_user_id, _book_id))
 
 
-""" try/catch block for handling potential MySQL database errors """ 
+#\ The start of the program
 
 db = mysql.connector.connect(
     host='localhost',
@@ -132,11 +139,12 @@ while user_input !=4:
     if user_input == 3:
         input_user_id= validate_user()
         account_option= show_account_menu()
-#\ Had to look at the guide could not get it to work 
+
         while input_user_id !=3:
+
             if input_user_id == 1: 
                 show_wishlist(cursor,input_user_id)
-
+#/ show_wishlist ask to add a book when it should just show the what is in the wishlist 
             
             if input_user_id == 2: 
                 show_books_to_add(cursor,input_user_id)
