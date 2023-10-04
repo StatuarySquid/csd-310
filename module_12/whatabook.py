a0a1c709c5509228f5226519d7d2ce5b2014f445
StatuarySquid <141707098+StatuarySquid@users.noreply.github.com>
2023-10-03 19:20:34 -0500

    left on show_wishlist

--- module_12/whatabook.py
+++ module_12/whatabook.py
@@ -1,3 +1,4 @@
+import sys
 import mysql.connector
 from mysql.connector import Error
 
@@ -9,7 +10,7 @@ my_db= mysql.connector.connect(
 
 cursor=my_db.cursor()
 
-cursor.execute("SELECT book_id, book_name, details_v,author FROM book;")
+'''cursor.execute("SELECT book_id, book_name, details_v,author FROM book;")
 
 book_result= cursor.fetchall()
 
@@ -18,4 +19,62 @@ for book in book_result:
     print('The book name: {}'.format(book[1]))
     print('The book detail: {}'.format(book[2]))
     print('The author: {}'.format(book[3]))
-    print('')
+    print('')'''
+
+def show_menu():
+    print("\n Welcome to our menu")
+    print(" \n 1. Store Locations \n 2. View Books \n 3. My account"
+    )
+
+    try:
+        user_input= int(input('Enter number of menu \n1-Store \n2-Books\n3-Account \n4-Exit \n '))
+
+        return user_input
+    except ValueError:
+        print("Invalid input")
+
+        sys.exit(0)
+
+def show_books(cursor):
+    cursor.execute("SELECT book_id, book_name, author,details_v FROM book;")
+    book_result= cursor.fetchall()
+
+    for book in book_result:
+        print('Book name: {}'.format(book[1]))
+        print('Author: {}'.format(book[2]))
+        print('Description: {}'.format(book[3]))
+        print('')
+
+def show_locations(cursor):
+    cursor.execute("Select store_id,locale from store;")
+    store_result=cursor.fetchall()
+    for store in store_result:
+        print('Store Location:{}'.format(store[1]))
+
+def validate_user(): 
+
+    while True:
+        try:
+            user_input_id= int(input('Please provide us with your user ID: '))
+
+            if user_input_id <= 0 or user_input_id > 3:
+                print('Invalid ID')
+                sys.exit(0)
+            return user_input_id
+        except ValueError: 
+            print('That is an invalid input.')
+            continue
+
+def show_account_menu():
+    
+    while True:
+        try:
+            print('\nAccount Menu \n1. Wishlist \n2. Add Book \n3. Main Menu ')
+            user_input_account= int(input('Enter number of menu:  '))
+            return user_input_account
+        except ValueError:
+            print("That is invalid number")
+            continue
+    
+def show_wishlist(cursor,user_id)
+    cursor.
